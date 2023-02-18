f5d8fc7e51172827e2f047d7e3e9866077a2839b
SmruthikaMeda <79011375+SmruthikaMeda@users.noreply.github.com>
2023-02-18 15:11:56 +0530

    Update hello.cpp

--- main/hello.cpp
+++ main/hello.cpp
@@ -6,5 +6,6 @@ int main(void) {
   cout << "Hello, World\n";
   cout << "Hello, This is Smruthika\n";
   cout << "I have successfully built and run PES1UG20CS429\n";
+  cout << "This is the change made.\n";
   return 0;
 }
