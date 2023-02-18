243ba649691e15403353319c80e8e443a00bdbb0
SmruthikaMeda <79011375+SmruthikaMeda@users.noreply.github.com>
2023-02-18 15:11:17 +0530

    Update PES1UG20CS429.cpp

--- PES1UG20CS429.cpp
+++ PES1UG20CS429.cpp
@@ -3,6 +3,6 @@ using namespace std;
 
 int main()
 {
-    cout << "Hello World! SRN: PES1UG20CS429. This is the change." << endl;
+    cout << "Hello World! SRN: PES1UG20CS429." << endl;
     return 0;
 }
